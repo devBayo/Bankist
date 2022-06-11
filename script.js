718c7243bd5037750f440875b661c4303199a503
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-06-11 22:29:24 +0000

    created accounts

--- script.js
+++ script.js
@@ -5,10 +5,31 @@ const user = document.querySelector('.user');
 const pin = document.querySelector('.pin');
 const app = document.querySelector('main');
 
-const account = {
+const account1 = {
   user: 'John Doe',
-  pin: 1234,
+  pin: 1111,
+  movements: [200, -400, 5000, 3600, -7000, 1000],
 };
+
+const account2 = {
+  user: 'Mike Bellion',
+  pin: 2222,
+  movements: [4000, -6000, 3000, -2500, -7000, 10000],
+};
+
+const account3 = {
+  user: 'Adam Smith',
+  pin: 3333,
+  movements: [20, -400, 500, 8000, -70, 1000],
+};
+
+const account4 = {
+  user: 'Samuel Jackson',
+  pin: 4444,
+  movements: [1000, -300, -5000, 9000, -700, -1000],
+};
+
+const accounts = [account1, account2, account3, account4];
 
 const shortenUser = function (username) {
   return `${username[0] + username[username.indexOf(' ') + 1]}`.toLowerCase();
